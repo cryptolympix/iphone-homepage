9cc604a8bfd52702e6988b3eadb3c0e7f71b6491
Maxime Joly <maxime.joly2808@gmail.com>
2020-06-14 15:48:14 +0200

    Fix sub navigation issue

--- styles/js/sub-nav-scroll.js
+++ styles/js/sub-nav-scroll.js
@@ -5,9 +5,11 @@ $(document).ready(function () {
     if ($(window).scrollTop() >= 50) {
       $('#sub-nav-lg-md').addClass('fixed');
       $('#sub-nav-sm-xs').addClass('fixed');
+      $('#section-1').css('padding-top', '95px');
     } else {
       $('#sub-nav-lg-md').removeClass('fixed');
       $('#sub-nav-sm-xs').removeClass('fixed');
+      $('#section-1').css('padding-top', '50px');
     }
   });
 });
